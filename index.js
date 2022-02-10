772623f7aa1ec24f99db96ac83ee2f9ee8ca4ceb
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-02-10 21:01:25 +0000

    Some random words actually working

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const readLine = require('readline-sync');
-const rword = require('rword');
+const readLine = require('readline-sync'); //Allows for user input
+const { rword } = require('rword'); // Random word generator
+
+// Generate 15 different 5 letter words
+let wordGen = rword.generate(15, { length: 5 });
+
+// Randomly choose from the array of 15 words
+let word = wordGen[Math.floor(Math.random(14)*wordGen.length)];
+
+// Sleep does sleep things, pauses the code.
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}; //Allows the code to pause, with promisies, of course.
+
