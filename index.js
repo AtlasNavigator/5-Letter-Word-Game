1701b4f5f75dfe92d0d2fa3d186d1d5e911c7991
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-02-10 21:27:43 +0000

    IT WORKS!

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 const readLine = require('readline-sync'); //Allows for user input
 const { rword } = require('rword'); // Random word generator
 
-// Generate 15 different 5 letter words
-let wordGen = rword.generate(15, { length: 5 });
+let answer = getAnswer()
+let playing=true;
 
-// Randomly choose from the array of 15 words
-let word = wordGen[Math.floor(Math.random(14)*wordGen.length)];
+while (playing){
+  guess = getGuess()
+  playing=checkWin(answer, guess);
+  if (playing == false){
+    continue
+  }
+  checkCommonLetters(answer, guess);
+  checkSameLetters(answer, guess);
+}
+console.log('Thanks for playing!')
 
-// Sleep does sleep things, pauses the code.
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}; //Allows the code to pause, with promisies, of course.
 
+function getAnswer(){
+  // Generate 15 different 5 letter words
+  let wordGen = rword.generate(15, { length: 5 });
+  let randomNumber = Math.floor(Math.random()*wordGen.length)
+  let answer = wordGen[randomNumber].split("")
+  return answer
+}
+
+function getGuess(){
+  let guess = readLine.question("Enter your 5 letter word guess: ");
+  while (guess.length !=5 ){
+    console.log('Invalid length.')
+    guess = readLine.question("Enter your 5 letter word guess: ");
+  }
+  guess = guess.split("");
+  return guess
+}
+
+function checkWin(answer, guess){
+   if (guess.join() == answer.join()){
+    console.log('You won!')
+    return false
+   }
+   return true
+}
+
+function checkCommonLetters(answer, guess){
+  let countSimilar = 0;
+  for (let i=0; i<answer.length; i++){
+    for (let j=0; j<guess.length; j++){
+      if (answer[i]==guess[j]){
+        countSimilar++
+      }
+    }
+  }
+  console.log(`${countSimilar} letter(s) in YOUR guess are also in MY SECRET word`);
+}
+
+function checkSameLetters(answer, guess){
+  let correctPosition = 0;
+  for (let i=0; i<answer.length; i++){
+    if (answer[i]==guess[i]){
+      correctPosition++
+    }
+  }
+  console.log(`${correctPosition} letter(s) in YOUR guess are in the correct position`);
+}
